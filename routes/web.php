d5b491b5a82dd619d31cf2c2af32569cc7412663
TheBlinkMan <cardosoalanf@gmail.com>
2018-04-15 19:54:41 -0300

    Update welcome view to show the content of the scraped data.
    Update welcome view to show the content of the scraped data.
    Use Goutte Facade to scrape data from camara legislativa.
    http://www2.camara.leg.br/transparencia/licitacoes-e-contratos/editais/pregaoeletronico

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,35 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $crawler = Goutte::request('GET', 'http://www2.camara.leg.br/transparencia/licitacoes-e-contratos/editais/pregaoeletronico');
+
+    $biddings = $crawler->filter('.cor')->each(function ($node) {
+
+        $number = $node->filterXPath('//tbody[2]/tr[1]/td[1]')->text();
+
+        $object = $node->filterXPath('//tbody[2]/tr[1]/td[2]')->text();
+
+        $opening_date = $node->filterXPath('//tbody[2]/tr[1]/td[3]')->text();
+
+        $last_update = $node->filterXPath('//tbody[2]/tr[1]/td[4]')->text();
+
+        $auction_notice = $node->filterXPath('//tbody[2]/tr[2]/td[2]/p[1]/a')->attr('href');
+        $proposal_model = $node->filterXPath('//tbody[2]/tr[2]/td[2]/p[2]/a')->attr('href');
+
+        $bidding = array(
+            "number" => $number,
+            "object" => $object,
+            "opening_date" => $opening_date,
+            "last_update" => $last_update,
+            "auction_notice" => $auction_notice,
+            "proposal_model" => $proposal_model,
+        );
+
+        return $bidding;
+    });
+
+    return view('welcome', [
+        'institution' => 'Câmara dos Deputados',
+        'biddings' => $biddings,
+    ]);
 });
